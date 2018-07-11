b29401888b0b4575a73b72195205ee629158a34e
saymynameww <wangxk0624@163.com>
2018-07-11 16:56:06 +0800

    0711

---
+++ Code/071101/run1_generate_feature.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  4 19:58:58 2018
+
+@author: ASUS
+"""
+
+import os
+import time
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import warnings
+warnings.filterwarnings("ignore")
+
+time_start = datetime.now()
+print('Start time:',time_start.strftime('%Y-%m-%d %H:%M:%S'))
+
+data_path = os.path.join(os.pardir,os.pardir,os.pardir, '1DF-data/preprocessed_data/')
+train_path = os.path.join(os.pardir,os.pardir,os.pardir, '1DF-data/train_and_test/train.csv')
+test_path = os.path.join(os.pardir,os.pardir,os.pardir, '1DF-data/train_and_test/test.csv')
+#-------agg特征-------
+#train_agg = pd.read_csv(data_path + 'train_agg.csv')
+#test_agg = pd.read_csv(data_path + 'test_agg.csv')
+train_agg = pd.read_csv(data_path + 'train_agg.csv',nrows=500)
+test_agg = pd.read_csv(data_path + 'test_agg.csv',nrows=500)
+agg = pd.concat([train_agg,test_agg],copy=False)
+
+#train_flg = pd.read_csv(data_path + 'train_flg.csv')
+train_flg = pd.read_csv(data_path + 'train_flg.csv',nrows=500)
+test_flg = pd.DataFrame(test_agg['USRID'])
+test_flg['FLAG'] = -1
+flg = pd.concat([train_flg,test_flg],copy=False)
+
+#-------log特征-------
+#train_log = pd.read_csv(data_path + 'train_log.csv')
+#test_log = pd.read_csv(data_path + 'test_log.csv')
+train_log = pd.read_csv(data_path + 'train_log.csv',nrows=500)
+test_log = pd.read_csv(data_path + 'test_log.csv',nrows=500)
+log = pd.concat([train_log,test_log],copy=False)
+#点击模块特征
+end_time = '2018-03-31 23:59:59'
+end_time = time.mktime(time.strptime(end_time, "%Y-%m-%d %H:%M:%S"))
+begin_time = '2018-03-01 00:00:00'
+begin_time = time.mktime(time.strptime(begin_time, "%Y-%m-%d %H:%M:%S"))
+log['time'] = log['OCC_TIM'].apply(lambda x:time.strptime(x, "%Y-%m-%d %H:%M:%S"))
+log['total_second'] = log['time'].apply(lambda x:time.mktime(x))-begin_time
+log = log.sort_values(['USRID','total_second'])
+log['total_minute'] = (log['total_second']/60).apply(np.int)
+log['total_hour'] = (log['total_minute']/60).apply(np.int)
+log['day'] = log['time'].apply(lambda x:x.tm_mday)
+log['hour'] = log['time'].apply(lambda x:x.tm_hour)
+log['weekday'] = log['time'].apply(lambda x:x.tm_wday) + 1
+log['next_second'] = log.groupby(['USRID'])['total_second'].diff(-1).apply(np.abs)
+log[['EVT_1','EVT_2','EVT_3']] = log['EVT_LBL'].str.split('-',expand=True)
+label_encoder = LabelEncoder()
+integer_encoded1 = label_encoder.fit_transform(log.EVT_1)
+integer_encoded2 = label_encoder.fit_transform(log.EVT_2)
+integer_encoded3 = label_encoder.fit_transform(log.EVT_3)
+integer_encoded1 = integer_encoded1.reshape(len(integer_encoded1), 1)
+integer_encoded2 = integer_encoded2.reshape(len(integer_encoded2), 1)
+integer_encoded3 = integer_encoded3.reshape(len(integer_encoded3), 1)
+onehot_encoder = OneHotEncoder(sparse=False)
+onehot_encoded1 = onehot_encoder.fit_transform(integer_encoded1)
+onehot_encoded2 = onehot_encoder.fit_transform(integer_encoded2)
+onehot_encoded3 = onehot_encoder.fit_transform(integer_encoded3)
+log_array = np.hstack((log.values,onehot_encoded1,onehot_encoded2,onehot_encoded3))
+EVT_list = np.hstack((['EVT_1_'+str(x) for x in range(onehot_encoded1.shape[1])],['EVT_2_'+str(x) for x in range(onehot_encoded2.shape[1])],['EVT_3_'+str(x) for x in range(onehot_encoded3.shape[1])]))
+log_cols = np.hstack((log.columns,EVT_list))
+log = pd.DataFrame(log_array,columns=log_cols)
+log = log.infer_objects()
+
+#行为时间特征
+day_info = log.groupby(['USRID'],as_index=False)['day'].agg({'day_mean':np.mean,'day_std':np.std,'day_min':np.min,'day_max':np.max,'day_skew':lambda x: pd.Series.skew(x),'day_kurt':lambda x: pd.Series.kurt(x)}) 
+day_info['day_rest'] = (day_info['day_max']-31).apply(np.abs)
+day_info['duration'] = (day_info['day_max']-day_info['day_min'])+1
+hour_info = log.groupby(['USRID'],as_index=False)['total_hour'].agg({'hour_mean':np.mean,'hour_std':np.std,'hour_min':np.min,'hour_max':np.max,'hour_skew':lambda x: pd.Series.skew(x),'hour_kurt':lambda x: pd.Series.kurt(x)}) 
+hour_info['hour_rest'] = (hour_info['hour_max']-np.int(end_time/3600)).apply(np.abs)
+#行为相隔时间特征
+next_time = log.groupby(['USRID'],as_index=False)['next_second'].agg({'next_second_mean':np.mean,'next_second_std':np.std,'next_second_min':np.min,'next_second_max':np.max,'next_second_skew':lambda x: pd.Series.skew(x),'next_second_kurt':lambda x: pd.Series.kurt(x)}) 
+next_time['next_minute_mean'] = next_time['next_second_mean']/60
+next_time['next_minute_std'] = next_time['next_second_std']/60
+next_time['next_minute_min'] = next_time['next_second_min']/60
+next_time['next_minute_max'] = next_time['next_second_max']/60
+next_time['next_minute_skew'] = next_time['next_second_skew']/60
+next_time['next_minute_kurt'] = next_time['next_second_kurt']/60
+next_time['next_hour_mean'] = next_time['next_minute_mean']/60
+next_time['next_hour_std'] = next_time['next_minute_std']/60
+next_time['next_hour_min'] = next_time['next_minute_min']/60
+next_time['next_hour_max'] = next_time['next_minute_max']/60
+next_time['next_hour_kurt'] = next_time['next_minute_kurt']/60
+next_time['next_hour_skew'] = next_time['next_minute_mean']/60
+next_time['next_day_mean'] = next_time['next_hour_mean']/24
+next_time['next_day_std'] = next_time['next_hour_std']/24
+next_time['next_day_min'] = next_time['next_hour_min']/24
+next_time['next_day_max'] = next_time['next_hour_max']/24
+next_time['next_day_skew'] = next_time['next_hour_skew']/24
+next_time['next_day_kurt'] = next_time['next_hour_kurt']/24
+day_unique = log.groupby(['USRID','day']).agg({'USRID':'mean','day':'mean'}).rename({'day': 'day_unique'},axis=1)
+day_unique['next_day_unique'] = day_unique.groupby(['USRID'])['day_unique'].diff(-1).apply(np.abs)
+next_day_unique = day_unique.groupby(['USRID'],as_index=False)['next_day_unique'].agg({'next_day_unique_mean':np.mean,'next_day_unique_std':np.std,'next_day_unique_min':np.min,'next_day_unique_max':np.max,'next_day_unique_skew':lambda x: pd.Series.skew(x),'next_day_unique_kurt':lambda x: pd.Series.kurt(x)}) 
+#滑窗 窗口内行为次数
+EVT_count_31to31 = log.loc[log['day']>=31].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_31to31':'count'})
+EVT_count_30to31 = log.loc[log['day']>=30].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_30to31':'count'})
+EVT_count_29to31 = log.loc[log['day']>=29].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_29to31':'count'})
+EVT_count_25to31 = log.loc[log['day']>=25].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_25to31':'count'})
+EVT_count_18to31 = log.loc[log['day']>=18].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_18to31':'count'})
+EVT_count_11to31 = log.loc[log['day']>=11].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_11to31':'count'})
+EVT_count_4to31 = log.loc[log['day']>=4].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_4to31':'count'})
+EVT_count_1to31 = log.loc[log['day']>=1].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_1to31':'count'})
+EVT_count_1to3 = log.loc[(log['day']>=1)&(log['day']<=3)].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_1to3':'count'})
+EVT_count_1to7 = log.loc[(log['day']>=1)&(log['day']<=7)].groupby(['USRID'],as_index=False)['EVT_LBL'].agg({'EVT_count_1to7':'count'})
+#按天统计行为次数特征
+everyday_count_temp = log.groupby(['USRID','day'],as_index=False)['day'].agg({'everyday_count':'count'})
+everyday_count = everyday_count_temp.groupby(['USRID'],as_index=False)['everyday_count'].agg({'everyday_count_mean':np.mean,'everyday_count_std':np.std,'everyday_count_min':np.min,'everyday_count_max':np.max,'everyday_count_skew':lambda x: pd.Series.skew(x),'everyday_count_kurt':lambda x: pd.Series.kurt(x)})
+#按小时统计行为次数特征
+everyhour_count_temp = log.groupby(['USRID','total_hour'],as_index=False)['total_hour'].agg({'everyhour_count':'count'})
+everyhour_count = everyhour_count_temp.groupby(['USRID'],as_index=False)['everyhour_count'].agg({'everyhour_count_mean':np.mean,'everyhour_count_std':np.std,'everyhour_count_min':np.min,'everyhour_count_max':np.max,'everyhour_count_skew':lambda x: pd.Series.skew(x),'everyhour_count_kurt':lambda x: pd.Series.kurt(x)})
+#最多行为在哪天
+most_EVT_day = everyday_count_temp.groupby(['USRID'],as_index=False).apply(lambda x: x[x.everyday_count==x.everyday_count.max()]).rename({'day':'most_EVT_day'},axis=1).drop('everyday_count',axis=1)
+most_EVT_day['most_EVT_day_sub_end_day'] = (most_EVT_day['most_EVT_day']-31).apply(np.abs)
+#最多行为在周几------
+#周几数量和占比------
+weekday_temp = log.groupby(['USRID','weekday'],as_index=False)['weekday'].agg({'weekday_count':'count'})
+MON_count = weekday_temp[weekday_temp.weekday==1].drop('weekday',axis=1).rename({'weekday_count':'MON_count'},axis=1)
+TUES_count = weekday_temp[weekday_temp.weekday==2].drop('weekday',axis=1).rename({'weekday_count':'TUES_count'},axis=1)
+WED_count = weekday_temp[weekday_temp.weekday==3].drop('weekday',axis=1).rename({'weekday_count':'WED_count'},axis=1)
+THUR_count = weekday_temp[weekday_temp.weekday==4].drop('weekday',axis=1).rename({'weekday_count':'THUR_count'},axis=1)
+FRI_count = weekday_temp[weekday_temp.weekday==5].drop('weekday',axis=1).rename({'weekday_count':'FRI_count'},axis=1)
+SAT_count = weekday_temp[weekday_temp.weekday==6].drop('weekday',axis=1).rename({'weekday_count':'SAT_count'},axis=1)
+SUN_count = weekday_temp[weekday_temp.weekday==7].drop('weekday',axis=1).rename({'weekday_count':'SUN_count'},axis=1)
+weekday_count = pd.merge(EVT_count_1to31,MON_count,on=['USRID'],how='left',copy=False)
+weekday_count = pd.merge(weekday_count,TUES_count,on=['USRID'],how='left',copy=False)
+weekday_count = pd.merge(weekday_count,WED_count,on=['USRID'],how='left',copy=False)
+weekday_count = pd.merge(weekday_count,THUR_count,on=['USRID'],how='left',copy=False)
+weekday_count = pd.merge(weekday_count,FRI_count,on=['USRID'],how='left',copy=False)
+weekday_count = pd.merge(weekday_count,SAT_count,on=['USRID'],how='left',copy=False)
+weekday_count = pd.merge(weekday_count,SUN_count,on=['USRID'],how='left',copy=False)
+weekday_count['MON_pct'] = weekday_count['MON_count']/weekday_count['EVT_count_1to31']
+weekday_count['TUES_pct'] = weekday_count['TUES_count']/weekday_count['EVT_count_1to31']
+weekday_count['WED_pct'] = weekday_count['WED_count']/weekday_count['EVT_count_1to31']
+weekday_count['THUR_pct'] = weekday_count['THUR_count']/weekday_count['EVT_count_1to31']
+weekday_count['FRI_pct'] = weekday_count['FRI_count']/weekday_count['EVT_count_1to31']
+weekday_count['SAT_pct'] = weekday_count['SAT_count']/weekday_count['EVT_count_1to31']
+weekday_count['SUN_pct'] = weekday_count['SUN_count']/weekday_count['EVT_count_1to31']
+weekday_count = weekday_count.drop('EVT_count_1to31',axis=1)
+#各TYP数量和占比
+TYP_temp = log.groupby(['USRID','TCH_TYP'],as_index=False)['TCH_TYP'].agg({'TYP_count':'count'})
+TYP0_count = TYP_temp[TYP_temp.TCH_TYP==0].drop('TCH_TYP',axis=1).rename({'TYP_count':'TYP0_count'},axis=1)
+TYP1_count = TYP_temp[TYP_temp.TCH_TYP==1].drop('TCH_TYP',axis=1).rename({'TYP_count':'TYP1_count'},axis=1)
+TYP_count = pd.merge(EVT_count_1to31,TYP0_count,on=['USRID'],how='left',copy=False)
+TYP_count = pd.merge(TYP_count,TYP1_count,on=['USRID'],how='left',copy=False)
+TYP_count['TYP0_pct'] = TYP_count['TYP0_count']/TYP_count['EVT_count_1to31']
+TYP_count['TYP1_pct'] = TYP_count['TYP1_count']/TYP_count['EVT_count_1to31']
+TYP_count = TYP_count.drop('EVT_count_1to31',axis=1)
+#ONE-HOT EVT 统计
+EVT_count = pd.DataFrame(agg['USRID'])
+for evt in EVT_list:
+    EVT_count_temp = log[log[evt]==1].groupby(['USRID'],as_index=False)[evt].agg({evt+'_count':'count'})
+    EVT_count = pd.merge(EVT_count,EVT_count_temp,on=['USRID'],how='left',copy=False)
+    
+#MERGE
+data = pd.merge(flg,agg,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,day_info,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,hour_info,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,next_time,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,next_day_unique,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_31to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_30to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_29to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_25to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_18to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_11to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_4to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_1to31,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_1to3,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count_1to7,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,everyday_count,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,everyhour_count,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,most_EVT_day,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,TYP_count,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,weekday_count,on=['USRID'],how='left',copy=False)
+data = pd.merge(data,EVT_count,on=['USRID'],how='left',copy=False)
+data['avg_EVT'] = data['EVT_count_1to31']/data['duration']
+#data = data.fillna(0) #特定列fillna，有些列不需要
+
+train_data = data[data['FLAG']!=-1]
+test_data = data[data['FLAG']==-1]
+train_data.to_csv(train_path,index=False)
+test_data.to_csv(test_path,index=False)
+
+time_end = datetime.now()
+print('End time:',time_end.strftime('%Y-%m-%d %H:%M:%S'))
+print('Total time:',"%.2f" % ((time_end-time_start).seconds/60),'minutes')
---
+++ Code/071101/run2_lgb.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 16:43:35 2018
+
+@author: ASUS
+"""
+
+import os
+import sys
+from datetime import datetime
+import time
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_auc_score
+from scipy.stats import pearsonr
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+from save_log import Logger
+
+def train_cv(params):
+    N = 5
+    model_i = 1
+    print('All params:',params)
+    skf = StratifiedKFold(n_splits=N,shuffle=False,random_state=624)
+    for train_in,test_in in skf.split(train_feature,train_label):
+        X_train,X_test,y_train,y_test = train_feature[train_in],train_feature[test_in],train_label[train_in],train_label[test_in]
+    
+        lgb_train = lgb.Dataset(X_train, y_train)
+        lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+    
+        gbm = lgb.train(params,
+                        lgb_train,
+                        num_boost_round=10000,
+                        valid_sets=lgb_eval,
+                        verbose_eval=False,
+                        early_stopping_rounds=50)
+    
+        gbm.save_model(model_path+'model_'+str(model_i)+'.txt')
+        model_i += 1
+    
+        y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
+        cv_roc.append(roc_auc_score(y_test,y_pred))
+        cv_prediction.append(gbm.predict(test_feature, num_iteration=gbm.best_iteration))
+
+def predict_func():
+    best_cv = 0
+    best_cv_roc = cv_roc[0]
+    best_cv_prediction = cv_prediction[0]
+    for i in range(1,len(cv_roc)):
+        if cv_roc[i] > best_cv_roc:
+            best_cv = i
+            best_cv_roc = cv_roc[i]
+            best_cv_prediction = cv_prediction[i]
+    print('best_cv:'+str(best_cv)+' best_cv_roc:',best_cv_roc)
+    result = pd.DataFrame()
+    result['USRID'] = list(test_userid.values)
+    result['RST'] = list(best_cv_prediction)
+    time_date = time.strftime('%m-%d-%H-%M',time.localtime(time.time()))
+    submit_file_name = '%s_%s.csv'%(str(time_date),str(best_cv_roc).split('.')[1])
+    result.to_csv(submit_file_name,index=False,sep='\t')
+    
+    gbm = lgb.Booster(model_file=model_path+'model_'+str(best_cv)+'.txt')
+    if show_importance==1:
+        print('所用特征重要性：'+ str(list(gbm.feature_importance())))
+    fig, ax = plt.subplots(1, 1, figsize=[16, 40])
+    lgb.plot_importance(gbm, ax=ax, max_num_features=400)
+    plt.savefig('feature_importance.png')
+    print(submit_file_name+' 线上:{}')
+
+def feature_selection(feature_mode,R_threshold):
+    train_path = os.path.join(os.pardir,os.pardir,os.pardir, '1DF-data/train_and_test/train.csv')
+    test_path = os.path.join(os.pardir,os.pardir,os.pardir, '1DF-data/train_and_test/test.csv')
+    train = pd.read_csv(train_path)
+    test = pd.read_csv(test_path)
+    train.pop('USRID')
+    train_label = train.pop('FLAG')
+    col = train.columns
+    test_userid = test.pop('USRID')
+    test.pop('FLAG')
+    if feature_mode == 1:
+        print('Loading all the features and labels...')
+        train_feature = train[col].values
+        test_feature = test[col].values
+    elif feature_mode == 2:
+        print('Loading Pearson important features and label...')
+        pearson = []
+        cols = col.values.tolist()
+        for col_name in cols:
+            pearson.append(pearsonr(train.label, train[col_name]))
+        pearson = pd.DataFrame(pearson).rename({0:'R_value',1:'P_value'},axis=1)
+        pearson['Feature_name'] = cols
+        P_threshold = 0.05
+        used_feature = pearson[(pearson.P_value<=P_threshold) & ((pearson.R_value>=R_threshold)|(pearson.R_value<=-R_threshold))]
+        used_cols = used_feature.Feature_name.tolist()
+        train_feature = train[used_cols].values
+        test_feature = test[used_cols].values
+        print('R_threshold'+str(R_threshold)+' 特征数：'+ str(test_feature.shape[1]))
+    return train_feature,train_label,test_feature,test_userid
+
+if __name__ == "__main__":
+    time_start = datetime.now()
+    print('Start time:',time_start.strftime('%Y-%m-%d %H:%M:%S'))
+    model_path = os.path.join(os.pardir,os.pardir, 'Model/')
+    
+    feature_mode = 2
+    R_threshold = 0.05
+    #train_mode = 2
+    show_importance = 0
+    stdout_backup = sys.stdout
+    sys.stdout = Logger("train_info.txt")
+    print('\n')
+    train_feature,train_label,test_feature,test_userid = feature_selection(feature_mode,R_threshold)
+    #params = train_tune(train_mode)
+#    params = {'boosting_type': 'gbdt', 'objective': 'binary', 'metric': {'auc'}, 'num_leaves': 32, 'learning_rate': 0.01, 'feature_fraction': 0.9, 'bagging_fraction': 0.8, 'bagging_freq': 5, 'verbose': 0}
+    params = {'boosting_type': 'gbdt', 'objective': 'binary', 'metric': {'auc'}, 'num_leaves': 32, 'learning_rate': 0.01, 'verbose': 0}
+    cv_roc = []
+    cv_prediction = []
+    train_cv(params)
+    predict_func() 
+    sys.stdout = stdout_backup
+    time_end = datetime.now()
+    print('End time:',time_end.strftime('%Y-%m-%d %H:%M:%S'))
+    print('Total time:',"%.2f" % ((time_end-time_start).seconds/60),'minutes')
